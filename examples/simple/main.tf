df786624675f396e88042a2c470dff483f714467
Adrien Thebo <adrien@lagrange-automation.io>
2018-09-10 10:25:19 -0700

    Add example service accounts to simple example

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -18,5 +18,22 @@ module "service-accounts" {
   source = "../../"
   project_id = "${module.project-factory.project_id}"
   service_accounts = [
+    {
+      account_id     = "editor-service-account"
+      description    = "Service account with editor rights"
+      roles          = ["roles/editor"]
+      network_access = true
+    },
+    {
+      account_id     = "browser-service-account"
+      description    = "Service account with browser rights"
+      roles          = ["roles/browser"]
+      network_access = false
+    },
+    {
+      account_id     = "limited-service-account"
+      description    = "Service account with no special rights"
+      roles          = []
+    },
   ]
 }
